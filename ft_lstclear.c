71c501a0822d9b4f39be4f819e6df8a450214508
Naaz10 <109809806+Naaz10@users.noreply.github.com>
2022-09-20 16:58:07 +0400

    List clear not protected issue fixed

---
+++ ft_lstclear.c
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstclear.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: naazafrin <naazafrin@student.42.fr>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/08/23 20:31:09 by nafrin            #+#    #+#             */
+/*   Updated: 2022/08/27 21:34:52 by naazafrin        ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	ft_lstclear(t_list **lst, void (*del)(void*))
+{
+	t_list *tmp;
+
+	if(!del || !lst || !*lst)
+	{
+		return ;
+	}
+	while (lst && *lst)
+	{
+		tmp = (*lst)->next;
+		ft_lstdelone(*lst, del);
+		*lst = tmp;
+	}
+	
+}
+
